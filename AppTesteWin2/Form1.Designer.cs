23d63c6a7df96807dedd040485ffe15e4e2c49f3
AmandaVT <avalerio@telluria.com.br>
2021-05-07 14:05:45 -0300

    Form de Cadastros
    aplicação windows form para cadastro de pessoas com interação com o banco

---
+++ AppTesteWin2/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace AppTesteWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.TxtCPF = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btNovo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(178, 18);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(221, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro Simples de Exemplo";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitulo.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(34, 162);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 16);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome: ";
+            this.lblNome.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblNome.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNome.Location = new System.Drawing.Point(123, 159);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(276, 22);
+            this.TxtNome.TabIndex = 2;
+            this.TxtNome.TextChanged += new System.EventHandler(this.TxtNome_TextChanged);
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(34, 199);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(73, 16);
+            this.lblEndereco.TabIndex = 3;
+            this.lblEndereco.Text = "Endereço: ";
+            this.lblEndereco.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblEndereco.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(123, 196);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(276, 22);
+            this.txtEndereco.TabIndex = 4;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(34, 126);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(37, 16);
+            this.lblCPF.TabIndex = 5;
+            this.lblCPF.Text = "CPF:";
+            this.lblCPF.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TxtCPF
+            // 
+            this.TxtCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCPF.Location = new System.Drawing.Point(123, 123);
+            this.TxtCPF.Name = "TxtCPF";
+            this.TxtCPF.Size = new System.Drawing.Size(164, 22);
+            this.TxtCPF.TabIndex = 6;
+            this.TxtCPF.TextChanged += new System.EventHandler(this.TxtCPF_TextChanged);
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(34, 236);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(62, 16);
+            this.lblTelefone.TabIndex = 7;
+            this.lblTelefone.Text = "Telefone";
+            this.lblTelefone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblTelefone.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(123, 233);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(164, 22);
+            this.txtTelefone.TabIndex = 8;
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenero.Location = new System.Drawing.Point(34, 270);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(56, 16);
+            this.lblGenero.TabIndex = 9;
+            this.lblGenero.Text = "Gênero:";
+            this.lblGenero.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(123, 259);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 36);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(285, 11);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(99, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Não especificar";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(184, 11);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Não Binário";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(91, 11);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Masculino";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 11);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Feminino";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(95, 330);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 12;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(226, 330);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 13;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // btNovo
+            // 
+            this.btNovo.Location = new System.Drawing.Point(354, 330);
+            this.btNovo.Name = "btNovo";
+            this.btNovo.Size = new System.Drawing.Size(75, 23);
+            this.btNovo.TabIndex = 14;
+            this.btNovo.Text = "Novo";
+            this.btNovo.UseVisualStyleBackColor = true;
+            this.btNovo.Click += new System.EventHandler(this.BtnNovo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Código:";
+            // 
+            // txtCod
+            // 
+            this.txtCod.Enabled = false;
+            this.txtCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCod.Location = new System.Drawing.Point(123, 68);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(164, 22);
+            this.txtCod.TabIndex = 0;
+            this.txtCod.TextChanged += new System.EventHandler(this.txtCod_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 393);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btNovo);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblGenero);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.TxtCPF);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.TextBox TxtCPF;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btNovo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCod;
+    }
+}
+
---
+++ AppTesteWin2/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AppTesteWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            string strCon = @"persistsecurityinfo=True;server=localhost;user id=root;database=apptestewin; pwd=061127";
+            MySqlConnection cn;
+            try
+            {
+                cn = new MySqlConnection(strCon);
+                cn.Open();
+
+                MySqlCommand comd = new MySqlCommand();
+                comd.Connection = cn;
+                comd.CommandType = CommandType.Text;
+                comd.CommandText = "delete from cadastroSimples where codigo = '" + txtCod.Text + "';";
+                comd.ExecuteNonQuery();
+                MessageBox.Show("Registro apagado com sucesso!");
+                LimparTela();
+                TxtCPF.Text = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //MessageBox.Show("Não é possível deletar no momento");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (TxtCPF.Text == "" || TxtCPF.Text == null)
+            {
+                btnExcluir.Enabled = false;
+                btnSalvar.Enabled = false;
+                return;
+            }
+        }
+
+        private void BtnNovo_Click(object sender, EventArgs e)
+        {
+            LimparTela();
+            TxtCPF.Text = null;
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            string strCon = @"persistsecurityinfo=True;server=localhost;user id=root;database=apptestewin; pwd=061127";
+            MySqlConnection cn;
+            try
+            {
+                cn = new MySqlConnection(strCon);
+                cn.Open();
+                
+                MySqlCommand comd = new MySqlCommand();
+                comd.Connection = cn;
+                comd.CommandType = CommandType.Text;
+                if (txtCod.Text == "" || txtCod.Text == null)
+                {
+                    comd.CommandText = "insert into cadastroSimples(cpf, nome, endereco, genero, telefone) values('" + TxtCPF.Text + "', '" +
+                        TxtNome.Text + "', '" + txtEndereco.Text + "', " + lerComboBox() + ", '" + txtTelefone.Text + "'); ";
+                }
+                else
+                {
+                    comd.CommandText = "update cadastroSimples set cpf = '" + TxtCPF.Text + "', nome= '" + TxtNome.Text +
+                        "', endereco= '" + txtEndereco.Text + "', genero= " + lerComboBox() + ", telefone= '" + txtTelefone.Text +
+                        "' where codigo=" + int.Parse(txtCod.Text) + ";";
+                }
+                comd.ExecuteNonQuery();
+                MessageBox.Show("Registro salvo com sucesso!");
+                //adicionar código gerado
+                // comd.CommandText = "select codigo from cadastroSimples where cpf = '" + TxtCPF.Text + "';" ;
+                comd.CommandText = "select codigo from cadastroSimples where cpf = '" + TxtCPF.Text + "';";
+                MySqlDataReader rd;
+                rd = comd.ExecuteReader();
+                rd.Read();
+                txtCod.Text = rd.GetString(0);
+
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void TxtCPF_TextChanged(object sender, EventArgs e)
+        {
+            if (TxtCPF.Text == "" || TxtCPF.Text == null)
+            {
+                btnSalvar.Enabled = false;
+            }
+            else
+            {
+                btnSalvar.Enabled = true;
+            }
+            string strCon = @"persistsecurityinfo=True;server=localhost;user id=root;database=apptestewin; pwd=061127";
+            MySqlConnection cn = new MySqlConnection(strCon);
+            cn.Open();
+            MySqlCommand comd = new MySqlCommand();
+            comd.Connection = cn;
+            comd.CommandType = CommandType.Text;
+            comd.CommandText = "select * from cadastroSimples where cpf = '" + TxtCPF.Text + "';";
+            MySqlDataReader rd;
+            rd = comd.ExecuteReader();
+            if (rd.Read())
+            {
+                txtCod.Text = rd.GetInt32("codigo").ToString();
+                TxtNome.Text = rd.GetString("nome");
+                txtTelefone.Text = rd.GetString("telefone");
+                txtEndereco.Text = rd.GetString("endereco");
+                EscreverComboBox(rd.GetInt32("genero"));
+            }
+            else
+            {
+                LimparTela();
+            }
+            cn.Close();
+        }
+
+        private void TxtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int lerComboBox()
+        {
+            if (radioButton1.Checked)
+            {
+                return 0;
+            }
+            else if (radioButton2.Checked)
+            {
+                return 1;
+            }
+            else if (radioButton3.Checked)
+            {
+                return 2;
+            }else 
+            {
+                //inclusive se a radioButton4 estiver marcada
+                return 3;
+            }
+        }
+
+        private void EscreverComboBox(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    radioButton1.Checked = true;
+                    break;
+                case 1:
+                    radioButton2.Checked = true;
+                    break;
+                case 2:
+                    radioButton3.Checked = true;
+                    break;
+                case 3:
+                default:
+                    radioButton4.Checked = true;
+                    break;
+            }
+        }
+
+        private void LimparTela()
+        {
+            txtCod.Text = null;
+            TxtNome.Text = null;
+            txtEndereco.Text = null;
+            txtTelefone.Text = null;
+            radioButton4.Checked = true;
+        }
+
+        private void txtCod_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCod.Text == "" || txtCod.Text == null)
+            {
+                btnExcluir.Enabled = false;
+            }
+            else
+            {
+                btnExcluir.Enabled = true;
+            }
+        }
+    }
+}
