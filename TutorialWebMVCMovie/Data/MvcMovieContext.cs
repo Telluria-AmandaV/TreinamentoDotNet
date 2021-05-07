506b8257c285e6b016d56984871e3c66f73b12b2
AmandaVT <avalerio@telluria.com.br>
2021-05-07 13:58:49 -0300

    MVC Sistema Locadora
    Sistema de locadora para aplicação de conceitos de MVC em .Net, MySql e Entity Framework

---
+++ TutorialWebMVCMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using TutorialWebMVCMovie.Models;
+
+namespace TutorialWebMVCMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ TutorialWebMVCMovie/Migrations/20210504194457_newcolunmv2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutorialWebMVCMovie.Migrations
+{
+    public partial class newcolunmv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
